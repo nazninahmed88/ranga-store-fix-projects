f49eaa4d0b28c1d8977b44bd0d476c36058181e9
nazninrinkie <nazninrinky@gmail.com>
2021-09-14 20:03:22 +0600

    updated

--- js/app.js
+++ js/app.js
@@ -21,8 +21,8 @@ const showProducts = (products) => {
       <p> Rating Count: ${product.rating.count} </p>
       
       <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-secondary">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-primary">add to cart</button>
+      <button id="details-btn" class="btn btn-success">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
